469ea8a20b1f1444debb7b291044315c29b4c806
Nguyễn Tuấn <anhtuan14072002@gmail.com>
2024-01-30 18:32:08 +0700

    tuấn

--- js/demo2.js
+++ js/demo2.js
@@ -1,23 +1,44 @@
-// // // setTimeout(function(){
-// // //     alert("hi chào cậu");
-// // // },2000);// delay 2000ms
+// // // // setTimeout(function(){
+// // // //     alert("hi chào cậu");
+// // // // },2000);// delay 2000ms
 
 // var h = document.getElementById("number");
-// // h.innerText = "hi chào cậu"
+// // h.innerText = "hi chào cậu";
+// // h.innerHTML = "<i>hi chào cậu</i>";
+
+
 // var t = 10;
 // var it = setInterval(function(){
 //    // alert("hi chào cậu");
 //     // console.log(t);
-//     h.innerText = t;
+//     // h.innerText = t;
+//     h.innerHTML += `<li>${t}</li>`
 //     t--;
 //     if(t<0){
 //         clearInterval(it);
 //     }
 // },1000); // cứ 1s là làm 
+// // var min = 10;
+// // var sec = 0;
+// // var time = setInterval(function(){
+// //     console.log(`${min}:${sec}`);
+// //     sec--;
+// //     if(sec<0){
+// //         min--;
+// //         sec=59;
+// //     }
+// //     if(min<0){
+// //         clearInterval(time);
+// //     }
+// // },10);
+// var m = document.getElementById("min");
+// var s = document.getElementById("sec");
 // var min = 10;
 // var sec = 0;
 // var time = setInterval(function(){
-//     console.log(`${min}:${sec}`);
+//     //console.log(`${min}:${sec}`);
+//     m.innerText = min;
+//     s.innerText = sec;
 //     sec--;
 //     if(sec<0){
 //         min--;
@@ -26,21 +47,4 @@
 //     if(min<0){
 //         clearInterval(time);
 //     }
-// },10);
-var m = document.getElementById("min");
-var s = document.getElementById("sec");
-var min = 10;
-var sec = 0;
-var time = setInterval(function(){
-    //console.log(`${min}:${sec}`);
-    m.innerText = min;
-    s.innerText = sec;
-    sec--;
-    if(sec<0){
-        min--;
-        sec=59;
-    }
-    if(min<0){
-        clearInterval(time);
-    }
-},20);
+// },20);
---
+++ js/demo3.js
@@ -0,0 +1,14 @@
+var fs;
+function fan(){
+    var f = document.getElementById("fan");
+    var dg = 0;
+    setInterval(function(){
+    if(dg == 360) dg=0;
+    dg+=10;
+    f.style.transform = `rotate(${dg}deg)`;
+    },20);
+}
+function stopFan(){
+    clearInterval(fs);
+
+}
